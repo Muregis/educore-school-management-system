4c52f94c2b124b55b3170ca59d31c4c5d9253bb9
Muregis <i.am.muregi@gmail.com>
2026-05-11 17:51:05 +0300

    re

--- backend/src/routes/teachers.routes.js
+++ backend/src/routes/teachers.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import bcrypt from "bcryptjs";
 import { supabase } from "../config/supabaseClient.js";
 import { authRequired } from "../middleware/auth.js";
-import { requireRoles } from "../middleware/roles.js";
+import { requireRoles, requireDirector } from "../middleware/roles.js";
 
 const router = Router();
 router.use(authRequired);
