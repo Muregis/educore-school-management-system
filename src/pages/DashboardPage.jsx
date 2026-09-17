46684b809afcd4787c212813638b97fde4e6e4be
EduCore POC <educore-poc@localhost>
2026-09-17 15:36:01 +0300

    equipment

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
+import { useCurrentTerm } from "../hooks/useCurrentTerm";
 import { Msg } from "../components"; 
 import Card from "../components/ui/Card";
 import Badge from "../components/ui/Badge";
