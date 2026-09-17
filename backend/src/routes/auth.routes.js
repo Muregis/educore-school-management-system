f5ff66b3e962e7b6eb9bcb79bcef0d77fb8f0cb9
EduCore POC <educore-poc@localhost>
2026-09-18 02:33:31 +0300

    hoze

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import bcrypt from "bcryptjs";
 import { authRequired } from "../middleware/auth.js";
+import { supabase } from "../config/supabaseClient.js";
 import { logActivity } from "../helpers/activity.logger.js";
 import { requireRoles, requireDirector } from "../middleware/roles.js";
 import { authorize } from "../middleware/permissions.js";
